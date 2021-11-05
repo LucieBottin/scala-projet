6c29553257dbe194e5aa70d4951d2aac07b0127e
xulaing <khauv@live.fr>
2021-11-05 16:02:22 +0100

    parseLineAirports

--- src/main/scala/projet.scala
+++ src/main/scala/projet.scala
@@ -1,30 +1,39 @@
-import scala.collection.MapView 
+import scala.collection.MapView
 import scala.io.BufferedSource
 
-  object parser_airports {
+object projet extends App {
+  import scala.io.Source
+  def readData: List[String] = Source
+    .fromFile("resources/airports.csv", "utf-8")
+    .mkString
+    .split("\n")
+    .toList
+  def filterData(line: String, result: Vector[String] = Vector(), tmp: String = "", index: Int = 0, quote: Boolean = false): List[String] = (line,quote) match {
+      case (line, _) if line.length == 0 => result.toList
+      case (line, _) if index == line.length() && line(index - 1) == ',' => (result :+ "N/A").toList.filter{x => x != ""} // end line with missing data
+      case (line, _) if index == line.length() => (result :+ tmp).toList.filter{el : String => el != ""} // end line + add ""
+      case (line, false) if line(index) == '"' => filterData(line, result, tmp + "\"", index + 1, true) // quote detected
+      case (line, true) if line(index) == '"' && index != (line.length() - 1) && line(index + 1) == ',' => filterData(line, result :+ (tmp + "\""), "", index + 1, false) // add the whole string to re
+      case (line, false) if line(index) == ',' && index != (line.length() - 1) && line(index + 1) == ','  => filterData(line, result :+ tmp :+ "N/A", "", index + 1, false)  // missing data
+      case (line, false) if line(index) == ',' => filterData(line, result :+ tmp, "", index + 1, false)  // comma error : add nothing to res
+      case _ => filterData(line, result, tmp + line(index), index + 1, quote) // Add the char to the tmp accumulator and next char
+    }
+}
+
+object airports {
     case class Airports(id: String, ident: String, `type`: String, name: String, latitude_deg: String, longitude_deg: String,
       elevation_ft: String, continent: String, iso_country: String, iso_region: String, municipality: String, scheduled_service: String,
       gps_code: String, iata_code: String, local_code: String, home_link: String, wikipedia_link: String, keywords: String)
 
-    def parseLinesAirports(l : List[String]): String = {
-    val ui : List[Airports] = l.map{list => {
-    stringToAirports(projet.read_data)}
-  }.toList
-  println(ui)
-  val oui : String = "oui"
-    oui
-}
+    def parseLinesAirports(list : List[String]): List[Airports] = {
+      val ui = list.map{line => {
+          createAirports(projet.filterData(line))
+        }
+      }.toList
+      println(ui(1))
+      ui
+    }
 
-    def stringToAirports(l: List[String]): Airports = Airports(l(0), l(1), l(2), l(3), l(4), l(5), l(6), l(7), l(8), l(9),
+    def createAirports(l: List[String]): Airports = Airports(l(0), l(1), l(2), l(3), l(4), l(5), l(6), l(7), l(8), l(9),
       l(10), l(11), l(12), l(13), l(14), l(15), l(16), l(17))
-  }
-
-object projet extends App {
-  import scala.io.Source
-  def read_data : List[String] = {
-    println("id, ident, type, name, latitude_deg, longitude_deg, elevation_ft, continent, iso_country, iso_region, municipality, scheduled_service, gps_code, iata_code, local_code, home_link, wikipedia_link, keywords")
-    val dt : List[String]  = Source.fromFile("resources/airports.csv", "utf-8").getLines.toList
-    val dt2 : List[List[String]] = dt.map(_.split("\n").toList)
-    dt2.flatten
-  }
 }
--- src/test/scala/projet.scala
+++ src/test/scala/projet.scala
@@ -1,10 +1,150 @@
 import projet._
 
-class projet_test extends org.scalatest.FunSuite with  org.scalatest.Matchers {
+class projet_test extends org.scalatest.FunSuite with org.scalatest.Matchers {
   test("StringToAirports with N/A elements in the middle") {
-    val data = parser_airports.stringToAirports(List("29929","LFPR","closed","Guyancourt Airport","48.76029968261719",
-      "2.0625","541","EU","FR","FR-J","Guyancourt","no","LFPR","N/A","N/A","N/A","http://fr.wikipedia.org/wiki/A%C3%A9rodrome_de_Guyancourt","N/A"))
-    data should be (parser_airports.Airports("29929","LFPR","closed","Guyancourt Airport","48.76029968261719","2.0625",
-      "541","EU","FR","FR-J","Guyancourt","no","LFPR","N/A","N/A","N/A","http://fr.wikipedia.org/wiki/A%C3%A9rodrome_de_Guyancourt","N/A"))
+    val data = airports.createAirports(
+      List(
+        "29929",
+        "LFPR",
+        "closed",
+        "Guyancourt Airport",
+        "48.76029968261719",
+        "2.0625",
+        "541",
+        "EU",
+        "FR",
+        "FR-J",
+        "Guyancourt",
+        "no",
+        "LFPR",
+        "N/A",
+        "N/A",
+        "N/A",
+        "http://fr.wikipedia.org/wiki/A%C3%A9rodrome_de_Guyancourt",
+        "N/A"
+      )
+    )
+    data should be(
+      airports.Airports(
+        "29929",
+        "LFPR",
+        "closed",
+        "Guyancourt Airport",
+        "48.76029968261719",
+        "2.0625",
+        "541",
+        "EU",
+        "FR",
+        "FR-J",
+        "Guyancourt",
+        "no",
+        "LFPR",
+        "N/A",
+        "N/A",
+        "N/A",
+        "http://fr.wikipedia.org/wiki/A%C3%A9rodrome_de_Guyancourt",
+        "N/A"
+      )
+    )
+  }
+  test("parse line airports") {
+    val data = airports.parseLinesAirports(
+      List(
+        "\"29929\",\"LFPR\",\"closed\",\"Guyancourt Airport\",\"48.76029968261719\",\"2.0625\",\"541\",\"EU\",\"FR\",\"FR-J\",\"Guyancourt\",\"no\",\"LFPR\",\"N/A\",\"N/A\",\"N/A\",\"http://fr.wikipedia.org/wiki/A%C3%A9rodrome_de_Guyancourt\",\"N/A\""
+      )
+    )
+    data should be(
+      List(
+        airports.Airports(
+          "29929",
+          "LFPR",
+          "closed",
+          "Guyancourt Airport",
+          "48.76029968261719",
+          "2.0625",
+          "541",
+          "EU",
+          "FR",
+          "FR-J",
+          "Guyancourt",
+          "no",
+          "LFPR",
+          "N/A",
+          "N/A",
+          "N/A",
+          "http://fr.wikipedia.org/wiki/A%C3%A9rodrome_de_Guyancourt",
+          "N/A"
+        )
+      )
+    )
+  }
+}
+
+class TestParserCSV extends org.scalatest.FunSuite with org.scalatest.Matchers {
+  test("Empty string") {
+    val data = projet.filterData("")
+    data should be(List())
+  }
+
+  test("simple") {
+    val data = projet.filterData(
+      "\"29929\",\"LFPR\",\"closed\",\"Guyancourt Airport\",\"48.76029968261719\",\"2.0625\",\"541\",\"EU\",\"FR\",\"FR-J\",\"Guyancourt\",\"no\",\"LFPR\",\"N/A\",\"N/A\",\"N/A\",\"http://fr.wikipedia.org/wiki/A%C3%A9rodrome_de_Guyancourt\",\"N/A\""
+    )
+    data should be(
+      List(
+        "\"29929\"",
+        "\"LFPR\"",
+        "\"closed\"",
+        "\"Guyancourt Airport\"",
+        "\"48.76029968261719\"",
+        "\"2.0625\"",
+        "\"541\"",
+        "\"EU\"",
+        "\"FR\"",
+        "\"FR-J\"",
+        "\"Guyancourt\"",
+        "\"no\"",
+        "\"LFPR\"",
+        "\"N/A\"",
+        "\"N/A\"",
+        "\"N/A\"",
+        "\"http://fr.wikipedia.org/wiki/A%C3%A9rodrome_de_Guyancourt\"",
+        "\"N/A\""
+      )
+    )
+  }
+
+  test("List String") {
+    val data = projet.filterData("airport")
+    data should be(List("airport"))
+  }
+
+  test("No comma inside quote") {
+    val data = projet.filterData("\"avion\",5432,\"volant\"")
+    data should be(List("\"avion\"", "5432", "\"volant\""))
+  }
+
+  test("Comma inside quote") {
+    val data = projet.filterData("\"avion,volant\",3,\"comma, coucou\"")
+    data should be(List("\"avion,volant\"", "3", "\"comma, coucou\""))
+  }
+
+  test("Quote and comma inside quote") {
+    val data = projet.filterData(
+      "\"avion \"\"FR\"\" CDG\",-21.4249589508666992"
+    )
+    data should be(
+      List("\"avion \"\"FR\"\" CDG\"", "-21.4249589508666992")
+    )
+  }
+
+  test("Null elements in the middle") {
+    val data = projet.filterData("\"avion\",,,,2")
+    data should be(List("\"avion\"", "N/A", "N/A", "N/A", "2"))
+  }
+
+  test("Null elements at the end") {
+    val data = projet.filterData("salut,,,,,")
+    data should be(List("salut", "N/A", "N/A", "N/A", "N/A", "N/A"))
   }
 }
